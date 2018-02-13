5c9c9d97230be9d123b2dbb056052d486d184ed4
pifou <pifou@zabeth03.insecure.deule.net>
2018-02-13 14:56:14 +0000

    test ADC to Serial

--- TShirt/broadcast.c
+++ TShirt/broadcast.c
@@ -3,13 +3,22 @@
 #include <stdio.h>
 
 #include "serial.h"
+#include "analog.h"
 
 #define MAC_SIZE	6
 #define IPV4_SIZE	4
 
 int main(void)
 {
-init_printf();
-// Your code here !!
-return 0;
+    init_serial(9600);
+    ad_init(0x01);
+    while(1) 
+    {
+        int sm = ad_sample();
+        send_serial((sm/100)+48);
+        send_serial((sm/10-(sm/100)*10)+48);
+        send_serial((sm-((sm/10-(sm/100)*10))*10-(sm/100)*100)+48);
+        send_serial(' ');
+    }
+    return 0;
 }
