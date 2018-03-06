09dd1cf31051f81392d971e61377580bf6c06b3b
root <root@zabeth03.insecure.deule.net>
2018-03-06 14:54:00 +0000

    socket serveur tcp

--- Serveur/serveur.c
+++ Serveur/serveur.c
@@ -14,6 +14,33 @@
 
 /** Procedure principale **/
 
-int main(int argc,char **argv){
-return(0);
+int gestionClient(int s){
+	/* Obtient une structure de fichier */
+	FILE *dialogue=fdopen(s,"a+");
+	if(dialogue==NULL){ perror("gestionClient.fdopen"); exit(EXIT_FAILURE); }
+
+	/* Echo */
+	//char ligne[6] = "couco\n";
+	//while(fgets(ligne,6,dialogue)!=NULL)
+    	fprintf(dialogue,"> coucou\n");
+
+	/* Termine la connexion */
+	fclose(dialogue);
+	return 0;
 }
+
+int main(int argc,char *argv[])
+{
+	int s;
+	 
+	/* Lecture des arguments de la commande */
+	//analyseArguments(argc,argv);
+	char *service = argv[1];
+    
+	/* Initialisation du serveur */
+	s=initialisationServeur(service);
+	   
+	/* Lancement de la boucle d'ecoute */
+	boucleServeur(s,gestionClient);
+}
+
